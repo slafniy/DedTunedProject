fdffda22bd35d35c2d74850056a678fbbe166674
slafniy <slafniy@gmail.com>
2024-11-14 19:03:26 +0400

    Simulator - refactoring: added function to simulate one character

--- Data/Projects/CommonUtility/damage_calc/simulator.py
+++ Data/Projects/CommonUtility/damage_calc/simulator.py
@@ -1,30 +1,41 @@
 import typing as t
+from itertools import product
+
+import pandas as pd
 
 from character import Character
 from data_processing import process
 from weapon import TWO_HANDED_SWORD_0
 
 
-def simulate_combat(characters: t.List[Character], target_ac_list=(13,), rounds=5, iterations=500) -> t.List[dict]:
+def simulate_character(character, target_ac, rounds, iterations) -> t.List[dict]:
     data = []
 
-    for target_ac in target_ac_list:
-        for character in characters:
-            while True:  # levels iteration
-                print(f">> {character.name=} {character.level=}")
-                for iteration_number in range(iterations):
-                    for round_number in range(1, rounds + 1):
-                        round_data ={
-                            'iteration_number': iteration_number,
-                            'round_number': round_number,
-                            'name': character.name,
-                            'level': character.level,
-                            'target_ac': target_ac,
-                            'round_damage': character.play_round(target_ac)
-                        }
-                        data.append(round_data)
-                if not character.level_up():
-                    break
+    while True:  # levels iteration
+        print(f">> {character.name=} {character.level=}")
+        for iteration_number in range(iterations):
+            for round_number in range(1, rounds + 1):
+                round_data ={
+                    'iteration_number': iteration_number,
+                    'round_number': round_number,
+                    'name': character.name,
+                    'level': character.level,
+                    'target_ac': target_ac,
+                    'round_damage': character.play_round(target_ac)
+                }
+                data.append(round_data)
+        if not character.level_up():
+            break
+
+    return data
+
+
+def simulate_combat(characters: t.List[Character], target_ac_list=(13,), rounds=5, iterations=500) -> pd.DataFrame:
+    data = []
+
+    for character, target_ac in product(characters, target_ac_list):
+        data += simulate_character(character, target_ac, rounds, iterations)
+
     df = process(data)
     return df
 
