ba88102546f537473d8ede60c19f9b0e0008ede6
slafniy <slafniy@gmail.com>
2025-03-15 21:42:01 +0400

    tmp - Scroll of Battle Resurrection now also restores 50% hp.

--- open_toolkit.bat
+++ open_toolkit.bat
@@ -2,5 +2,6 @@ set project=DedTuned
 set toolkit_path="D:\SteamLibrary\steamapps\common\Baldurs Gate 3 Toolkit\"
 
 cd /D %toolkit_path%
-%toolkit_path%\Glasses.exe -project %project% -lvl WLD_Main_A
-::%toolkit_path%\Glasses.exe -project %project% -lvl Basic_Level_A
+
+::%toolkit_path%\Glasses.exe -project %project% -lvl WLD_Main_A
+%toolkit_path%\Glasses.exe -project %project% -lvl Basic_Level_A
