074fa54eca82aa14c8a1f17f4cd0b4b9c208c0c5
slafniy <slafniy@gmail.com>
2025-03-16 17:57:43 +0400

    added bg3lsx2lsf.exe integration to the toolkit launcher

--- open_toolkit.bat
+++ open_toolkit.bat
@@ -1,5 +1,10 @@
 set project=DedTuned
 set toolkit_path="D:\SteamLibrary\steamapps\common\Baldurs Gate 3 Toolkit\"
+set lsx2lsf="C:\Users\slafniy\Desktop\bg3modding\bg3lsx2lsf\bg3lsx2lsf.exe"
+
+set path_to_convert=%~dp0Public/DedTuned_de9f3db5-7ca9-a872-75d4-3cc4a09a90cd/RootTemplates
+
+%lsx2lsf% %path_to_convert%
 
 cd /D %toolkit_path%
 
