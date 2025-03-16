e60d64f2b6d6681185980f76ecc9cdd53709f6dc
slafniy <slafniy@gmail.com>
2025-03-17 01:09:57 +0400

    moved bg3lsx2lsf.exe to separate batch

---
+++ convert_root_templates.bat
@@ -0,0 +1,4 @@
+set lsx2lsf="C:\Users\slafniy\Desktop\bg3modding\bg3lsx2lsf\bg3lsx2lsf.exe"
+set path_to_convert=%~dp0Public/DedTuned_de9f3db5-7ca9-a872-75d4-3cc4a09a90cd/RootTemplates
+
+%lsx2lsf% %path_to_convert%
